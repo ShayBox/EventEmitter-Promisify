a0b88d80c28a31b84964ca1d3dce6067db7f2a85
Shayne Hartford (ShayBox) <shaybox@shaybox.com>
2019-07-22 01:02:05 -0400

    0.0.2 - Fix EventEmitter undefined

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const EventEmitter = require('events');
+
 Object.defineProperty(EventEmitter.prototype, 'promisify', {
   value: function(event) {
     return new Promise(resolve => this.once(event, resolve));
